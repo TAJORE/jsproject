9397a8de47f6d541cd00e08d5cec4f97957d527e
TAJORE <jouanirenatot@gmail.com>
2017-09-12 21:30:20 +0200

    intial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>learn js!</title>
+</head>
+<body>
+<p id="demo">JavaScript can change the style of an HTML element.</p>
+<button type="button" onclick="document.getElementById('demo').style.fontSize='35px'">Click here to display!</button>
+</body>
+<script src="js_project.js"></script>
+</html>
---
+++ js_project.js
@@ -0,0 +1,5 @@
+
+var start = 'good afternoon ', name, end = ' !', result;
+name = prompt('what is your name?');
+result = start + name + end;
+alert(result);
