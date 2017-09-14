82d5b666c2fdf31ad8b62934db9744a630395cac
TAJORE <jouanirenatot@gmail.com>
2017-09-14 12:32:39 +0200

    update school

--- index.php
+++ index.php
@@ -3,49 +3,50 @@
 <head>
     <meta charset="utf-8" />
     <link rel="stylesheet" href="school.css" />
+    <ul>
+        <li class="dropdown">
+            <span href="" class="dropbtn" id="admission" >Admissions</span>
+            <div class="dropdown-content">
+                <a href="#">Level 1</a>
+                <a href="#">Level 2</a>
+                <a href="#">Level 3</a>
+                <a href="#">Level 4</a>
+                <a href="#">Level 5</a>
+            </div>
+        </li>
+        <li class="dropdown">
+            <span href="" class="dropbtn" class="dropbtn" id="program" >Programs</span>
+            <div class="dropdown-content">
+                <a href="#">Level 1</a>
+                <a href="#">Level 2</a>
+                <a href="#">Level 3</a>
+                <a href="#">Level 4</a>
+                <a href="#">Level 5</a>
+            </div>
+        </li>
+        <li class="dropdown">
+            <span href="https://www.w3schools.com/js/default.asp" class="dropbtn" class="dropbtn" id="inscript" >Inscription</span>
+            <div class="dropdown-content">
+                <a href="#">Yaounde Campus</a>
+                <a href="#">Douala Campus</a>
+                <a href="#">Bafoussam Campus</a>
+                <a href="#">Ebolowa Campus</a>
+                <a href="#">Bafia Campus</a>
+            </div>
+        </li>
+        <li class="dropdown">
+            <a  class="dropbtn" class="dropbtn" id="tuto"  href="tutorial-js.html">Tutoriel JS </a>
+            <div class="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+            </div>
+        </li>
+    </ul>
 </head>
 <body>
   <div id="contenu">
-    <ul>
-    <li class="dropdown">
-        <span href="" class="dropbtn" id="admission" onclick="changer_admission()">Admissions</span>
-        <div class="dropdown-content">
-            <a href="#">Level 1</a>
-            <a href="#">Level 2</a>
-            <a href="#">Level 3</a>
-            <a href="#">Level 4</a>
-            <a href="#">Level 5</a>
-        </div>
-    </li>
-    <li class="dropdown">
-        <span href="" class="dropbtn" class="dropbtn" id="program" onclick="changer_program()">Programs</span>
-        <div class="dropdown-content">
-            <a href="#">Level 1</a>
-            <a href="#">Level 2</a>
-            <a href="#">Level 3</a>
-            <a href="#">Level 4</a>
-            <a href="#">Level 5</a>
-        </div>
-    </li>
-    <li class="dropdown">
-        <span href="https://www.w3schools.com/js/default.asp" class="dropbtn" class="dropbtn" id="inscript" onclick="changer_inscript()">Inscription</span>
-        <div class="dropdown-content">
-            <a href="#">Yaounde Campus</a>
-            <a href="#">Douala Campus</a>
-            <a href="#">Bafoussam Campus</a>
-            <a href="#">Ebolowa Campus</a>
-            <a href="#">Bafia Campus</a>
-        </div>
-    </li>
-    <li class="dropdown">
-        <a  class="dropbtn" class="dropbtn" id="tuto" onclick="changer_tuto()" href=" https://www.w3schools.com/code/tryit.asp?filename=FJJ5RJH9ADAK">Tutoriel JS </a>
-        <div class="dropdown-content">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
-        </div>
-    </li>
-    </ul>
+       test
   </div>
 </body>
 <script src="school.js"></script>
