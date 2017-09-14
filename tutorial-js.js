ec82b10c3816e7f2527a57d32c4ebb85f9da63db
TAJORE <jouanirenatot@gmail.com>
2017-09-15 01:36:47 +0200

    navbar school

--- tutorial-js.js
+++ tutorial-js.js
@@ -1,8 +1,15 @@
 
 //1
-function myFunction(name) {
-    return " bonjour et bien venue à " + name;
+function maFunction1(nom) {
+    return " bonjour et bien venue à " + nom;
 }
 
-document.getElementById("affichage_nom").innerHTML=myFunction("yaoundé");
+document.getElementById("affichage_nom").innerHTML=maFunction1("yaoundé");
 //2
+function formulaire_validation() {
+    var x = document.forms["monFormulaire"]["nomformulaire"].value;
+    if (x == "") {
+        alert("le nom doit obligatoirement etre saisi");
+        return false;
+    }
+}
