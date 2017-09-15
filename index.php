d4054b117facb2b303ba19be31515ae0a284de95
TAJORE <jouanirenatot@gmail.com>
2017-09-15 21:56:16 +0200

    navbar school

--- index.php
+++ index.php
@@ -3,7 +3,47 @@
 <head>
     <meta charset="utf-8" />
     <link rel="stylesheet" href="school.css" />
-    <?php include "nav.html";?>
+    <title>school project</title>
+    <ul>
+        <li class="dropdown">
+            <span href="" class="dropbtn" id="changer-couleur-admission" onClick="admission()">Admissions</span>
+            <div class="dropdown-content">
+                <a href="#">Level 1</a>
+                <a href="#">Level 2</a>
+                <a href="#">Level 3</a>
+                <a href="#">Level 4</a>
+                <a href="#">Level 5</a>
+            </div>
+        </li>
+        <li class="dropdown">
+            <span href="" class="dropbtn"  id="change-couleur-program" onClick="program()">Programs</span>
+            <div class="dropdown-content">
+                <a href="#">Level 1</a>
+                <a href="#">Level 2</a>
+                <a href="#">Level 3</a>
+                <a href="#">Level 4</a>
+                <a href="#">Level 5</a>
+            </div>
+        </li>
+        <li class="dropdown">
+            <span  class="dropbtn"   id="changer-couleurr-inscription" onClick="inscription()">Inscription</span>
+            <div class="dropdown-content">
+                <a href="#">Yaounde Campus</a>
+                <a href="#">Douala Campus</a>
+                <a href="#">Bafoussam Campus</a>
+                <a href="#">Ebolowa Campus</a>
+                <a href="#">Bafia Campus</a>
+            </div>
+        </li>
+        <li class="dropdown">
+            <a  class="dropbtn"    href="tutorial-js.html" id="changer-couleur-tuto" onClick="tuto()">Tutoriel JS </a>
+            <div class="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+            </div>
+        </li>
+    </ul>
 </head>
     <body>
   <div id="premier_contenu">
