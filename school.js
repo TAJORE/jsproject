62d478cf6078fab9008a059d5a99ce2fa82739c1
TAJORE <jouanirenatot@gmail.com>
2017-09-16 00:31:22 +0200

    navbar school

--- school.js
+++ school.js
@@ -1,25 +1,98 @@
 
+function accueil() {
+    var element = document.getElementById("changer-couleur-accueil");
+    element.style.backgroundColor='#9ACD32';
+}
 
 function admission() {
     var element = document.getElementById("changer-couleur-admission");
     element.style.backgroundColor='#9ACD32';
-
 }
-function inscription() {
-    var element = document.getElementById("changer-couleurr-inscription");
-    element.style.backgroundColor='#9ACD32';
 
-}
+
 function program() {
-    var element = document.getElementById("change-couleur-program");
+    var element = document.getElementById("changer-couleur-program");
     element.style.backgroundColor='#9ACD32';
+}
 
+function inscription() {
+    var element = document.getElementById("changer-couleur-inscription");
+    element.style.backgroundColor='#9ACD32';
 }
 function tuto() {
     var element = document.getElementById("changer-couleur-tuto");
     element.style.backgroundColor='#9ACD32';
+}
+
+function reset_bg_sauf_accueil(){
+    var element = document.getElementById("changer-couleur-admission");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-program");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-inscription");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-tuto");
+    element.style.backgroundColor='#333';
+}
+
+function reset_bg_sauf_admission() {
+    var element = document.getElementById("changer-couleur-accueil");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-program");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-inscription");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-tuto");
+    element.style.backgroundColor='#333';
 
 }
 
+function reset_bg_sauf_program() {
 
+    var element = document.getElementById("changer-couleur-accueil");
+    element.style.backgroundColor='#333';
 
+    var element = document.getElementById("changer-couleur-admission");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-inscription");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-tuto");
+    element.style.backgroundColor='#333';
+
+}
+
+function reset_bg_sauf_inscription(){
+    var element = document.getElementById("changer-couleur-accueil");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-admission");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-program");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-tuto");
+    element.style.backgroundColor='#333';
+}
+
+function reset_bg_sauf_tuto(){
+    var element = document.getElementById("changer-couleur-accueil");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-admission");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-program");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-inscription");
+    element.style.backgroundColor='#333';
+}
