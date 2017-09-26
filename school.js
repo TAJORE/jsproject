fe9cd97dc30294e22535ae50439c4fc36a10c569
TAJORE <jouanirenatot@gmail.com>
2017-09-26 20:52:38 +0200

    update

--- school.js
+++ school.js
@@ -23,6 +23,14 @@ function tuto() {
     var element = document.getElementById("changer-couleur-tuto");
     element.style.backgroundColor='#9ACD32';
 }
+ function tuto2(){
+     var element = document.getElementById("changer-couleur-tuto2");
+     element.style.backgroundColor= '#9ACD32';
+ }
+function tuto_oc(){
+    var element = document.getElementById("changer-couleur-tuto-oc");
+    element.style.backgroundColor= '#9ACD32';
+}
 
 function reset_bg_sauf_accueil(){
     var element = document.getElementById("changer-couleur-admission");
@@ -35,6 +43,12 @@ function reset_bg_sauf_accueil(){
     element.style.backgroundColor='#333';
 
     var element = document.getElementById("changer-couleur-tuto");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-tuto2");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-tuto-oc");
     element.style.backgroundColor='#333';
 }
 
@@ -49,6 +63,12 @@ function reset_bg_sauf_admission() {
     element.style.backgroundColor='#333';
 
     var element = document.getElementById("changer-couleur-tuto");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-tuto2");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-tuto-oc");
     element.style.backgroundColor='#333';
 
 }
@@ -67,6 +87,12 @@ function reset_bg_sauf_program() {
     var element = document.getElementById("changer-couleur-tuto");
     element.style.backgroundColor='#333';
 
+    var element = document.getElementById("changer-couleur-tuto2");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-tuto-oc");
+    element.style.backgroundColor='#333';
+
 }
 
 function reset_bg_sauf_inscription(){
@@ -81,6 +107,12 @@ function reset_bg_sauf_inscription(){
 
     var element = document.getElementById("changer-couleur-tuto");
     element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-tuto2");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-tuto-oc");
+    element.style.backgroundColor='#333';
 }
 
 function reset_bg_sauf_tuto(){
@@ -94,5 +126,51 @@ function reset_bg_sauf_tuto(){
     element.style.backgroundColor='#333';
 
     var element = document.getElementById("changer-couleur-inscription");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-tuto2");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-tuto-oc");
+    element.style.backgroundColor='#333';
+}
+
+function reset_bg_sauf_tuto2(){
+    var element = document.getElementById("changer-couleur-accueil");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-admission");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-program");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-inscription");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-tuto");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-tuto-oc");
+    element.style.backgroundColor='#333';
+}
+
+function reset_bg_sauf_tuto_oc(){
+    var element = document.getElementById("changer-couleur-accueil");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-admission");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-program");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-inscription");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-tuto");
+    element.style.backgroundColor='#333';
+
+    var element = document.getElementById("changer-couleur-tuto2");
     element.style.backgroundColor='#333';
 }
