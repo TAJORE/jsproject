873641f08c7fb44f5439dbb9d7fa1067a0e2f751
TAJORE <jouanirenatot@gmail.com>
2017-11-26 23:52:20 +0100

    update

--- test2.js
+++ test2.js
@@ -1,3 +1,29 @@
+//fonction utilisée pour désactivé les méssages d'érreurs
+
+function deactivateTooltips(){
+    var tooltips = document.querySelectorAll('.tooltip'),
+        tooltipsLength = tooltips.length;
+
+    for (var i = 0; i < tooltipsLength; i++){
+        tooltips[i].style.display = 'none';
+    }
+}
+//fonction qui identifie l'élément du formulaire sur lequel on travail
+
+function getTooltip(elements){
+    while (elements = elements.nextSibling){
+        if (elements.className ==='tooltip'){
+            return elements;
+        }
+    }
+    return false;
+}
+//fonction qui tranforme les texte en majuscule
+
+function ChangeCase(elem)
+{
+    elem.value = elem.value.toUpperCase();
+}
 function surligne(champ, erreur)
 {
     if(erreur)
@@ -36,7 +62,7 @@ function verifAge(champ)
 
     var age = parseInt(champ.value);
 
-    if(isNaN(age) || age < 5 || age > 111)
+    if(isNaN(age) || age < 15 || age > 111)
 
     {
 
@@ -59,27 +85,30 @@ function verifAge(champ)
 }
 
 function verifForm(f)
-
 {
-
     var pseudoOk = verifPseudo(f.pseudo);
-
+    var mailOk = verifMail(f.email);
     var ageOk = verifAge(f.age);
 
-
-
-    if(pseudoOk && ageOk)
-
+    if(pseudoOk && mailOk && ageOk)
         return true;
-
     else
-
     {
-
         alert("Veuillez remplir correctement tous les champs");
-
         return false;
-
     }
-
+}
+function verifMail(champ)
+{
+    var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+    if(!regex.test(champ.value))
+    {
+        surligne(champ, true);
+        return false;
+    }
+    else
+    {
+        surligne(champ, false);
+        return true;
+    }
 }
